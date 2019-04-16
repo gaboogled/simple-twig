8a9cfd21deb4612651182c24f49b4e3d5b42f2a6
gaboogled <gaboogled@gmail.com>
2019-04-16 06:12:52 -0700

    Setup

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+// webpack.config.js
+module.exports = {
+	...
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [{
+                loader: "style-loader"
+            }, {
+                loader: "css-loader"
+            }, {
+                loader: "sass-loader",
+                options: {
+                    includePaths: ["absolute/path/a", "absolute/path/b"]
+                }
+            }]
+        }]
+    }
+};
